49d61250edaa58b4ed3a4fdb2620f46aa0a6e31f
Andy <andybrummittps4@hotmail.com>
2022-04-13 19:40:49 +0100

    styled account page

--- src/components/AccountPage/AccountPage.component.jsx
+++ src/components/AccountPage/AccountPage.component.jsx
@@ -1,25 +1,36 @@
-import React, { useState } from "react";
-import { deleteAccount, handleSignOut } from "../../firebase/firebase-utils/firebase.auth.utils";
+import React, { useContext, useState } from "react";
+import { ThemeContext } from "../../App";
+import {
+  deleteAccount,
+  handleSignOut,
+} from "../../firebase/firebase-utils/firebase.auth.utils";
+import "./accountpage.styles.scss";
 
 const AccountPage = () => {
-  const [ showDeletePrompt, setShowDeletePrompt ] = useState(false);
-  const [ error, setError ] = useState(null);
+
+  const { darkMode } = useContext(ThemeContext);
+
+  const [showDeletePrompt, setShowDeletePrompt] = useState(false);
+  const [error, setError] = useState(null);
   const deleteAccountAndShowMessage = async () => {
-    deleteAccount()
-        .catch(error => setError(error))
-  }
+    deleteAccount().catch((error) => setError(error));
+  };
   return (
-    <div>
-    {error && <p>{error.message}</p>}
-      <ul>
+    <div className={`account-page-container ${darkMode ? 'dark' : ''}`}>
+      <h2>My Account</h2>
+      {error && <p>{error.message}</p>}
+      <ul className="account-options">
         <li onClick={handleSignOut}>Sign out</li>
         <li onClick={setShowDeletePrompt}>Delete Account</li>
       </ul>
       {showDeletePrompt && (
-          <div>
-              <p>Are you sure you want to delete your account?</p>
-              <button onClick={deleteAccountAndShowMessage}>Yes</button><button onClick={() => setShowDeletePrompt(false)}>No</button>
+        <div className="delete-prompt-container">
+          <p>Are you sure you want to delete your account?</p>
+          <div className="delete-options-btns">
+            <button onClick={deleteAccountAndShowMessage}>Yes</button>
+            <button onClick={() => setShowDeletePrompt(false)}>No</button>
           </div>
+        </div>
       )}
     </div>
   );
